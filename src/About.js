859f6f838295709b2d61ccee9c23a36630a0f3e2
jessicatriana <jessica.triana.tx@gmail.com>
2020-04-06 10:48:29 -0500

    Done.

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                 <hi>This is my about component!</hi>
+            </div>
+        );
+    }
+}
+
+export default About;
